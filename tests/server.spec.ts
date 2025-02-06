704576e8d0812fd83efea1a4856718064234f1ab
co6tter <34955642+co6tter@users.noreply.github.com>
2025-02-06 19:50:50 +0900

    feat: improve local server to return files (#1)

--- tests/server.spec.ts
+++ tests/server.spec.ts
@@ -2,18 +2,18 @@ import { expect, test } from "@playwright/test";
 
 test.describe("Server", () => {
   test("should return Hello World", async ({ request }) => {
-    const response = await request.get("http://localhost:3000");
+    const response = await request.get("http://localhost:3000/heartbeat");
     expect(response.ok()).toBeTruthy();
     expect(await response.text()).toBe("Hello World!\n");
   });
 
   test("should return 200 status code", async ({ request }) => {
-    const response = await request.get("http://localhost:3000");
+    const response = await request.get("http://localhost:3000/heartbeat");
     expect(response.status()).toBe(200);
   });
 
   test("should have correct content type", async ({ request }) => {
-    const response = await request.get("http://localhost:3000");
+    const response = await request.get("http://localhost:3000/heartbeat");
     expect(response.headers()["content-type"]).toBe("text/plain");
   });
 });
