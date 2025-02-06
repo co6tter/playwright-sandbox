fc96f762f31d47614749b421ff2c75939d33cdff
co6tter <34955642+co6tter@users.noreply.github.com>
2025-02-06 19:50:50 +0900

    feat: improve local server to return files (#1)

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # playwright-sandbox
 
+## Usage
+```bash
+$ npx playwright test --list
+$ npx playwright test
+$ npx playwright test --ui
+$ npx playwright show-report
+$ npx playwright codegen
+```
+
 ## Note
 - sample
--- server.mjs
+++ server.mjs
@@ -1,12 +1,42 @@
+import { readFile } from "node:fs";
 import { createServer } from "node:http";
+import { extname, join } from "node:path";
 
 const port = process.env.PORT || 3000;
+const baseDir = process.cwd();
 
 const server = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!\n");
+  console.log("req.url: ", req.url);
+  if (req.url === "/heartbeat") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    return res.end("Hello World!\n");
+  }
+
+  let filePath = req.url === "/" ? "/index.html" : req.url;
+  filePath = join(baseDir, "tests", filePath);
+  const ext = extname(filePath);
+  const contentType =
+    {
+      ".html": "text/html",
+      ".css": "text/css",
+      ".js": "text/javascript",
+      ".json": "application/json",
+      ".png": "image/png",
+      ".jpg": "image/jpeg",
+      ".gif": "image/gif",
+    }[ext] || "text/plain";
+
+  readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, "0.0.0.0", () => {
-  console.log(`Server running at http://0.0.0.0:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
