242ad319cc5d2a6957f38a7cdd4b2a28038e48a5
co6tter <34955642+co6tter@users.noreply.github.com>
2025-02-07 09:16:40 +0900

    feat: add table and some tests

--- tests/server.spec.ts
+++ tests/server.spec.ts
@@ -16,4 +16,19 @@ test.describe("Server", () => {
     const response = await request.get("http://localhost:3000/heartbeat");
     expect(response.headers()["content-type"]).toBe("text/plain");
   });
+
+  test("should be 'Hello World from HTML' in the h1", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+    await expect(page.locator("h1")).toHaveText("Hello World from HTML");
+  });
+
+  test("should be 'body 1-1' at row 1, column 1 in the table", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+    await expect(page.locator("table tbody tr").first().locator("td").first()).toHaveText("body 1-1");
+  });
+
+  test("should be 'body 2-4' at row 2, column 4 in the table", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+    await expect(page.locator("table tbody tr").nth(1).locator("td").nth(3)).toHaveText("body 2-4");
+  });
 });
