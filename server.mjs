0e13b1e5531ad6d0447184eeb82bb984a57e93d1
co6tter <co6tter@gmail.com>
2025-04-29 10:06:08 +0900

    feat: add basic auth

--- server.mjs
+++ server.mjs
@@ -1,12 +1,35 @@
+import { Buffer } from "node:buffer";
 import { readFile } from "node:fs";
 import { createServer } from "node:http";
 import { extname, join } from "node:path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 const baseDir = process.cwd();
 
+const USERNAME = process.env.BASIC_AUTH_USERNAME;
+const PASSWORD = process.env.BASIC_AUTH_PASSWORD;
+
 const server = createServer((req, res) => {
   console.log("req.url: ", req.url);
+
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Basic ")) {
+    res.writeHead(401, { "WWW-Authenticate": 'Basic realm="Secure Area"' });
+    return res.end("Unauthorized");
+  }
+
+  const base64Credentials = authHeader.split(" ")[1];
+  const credentials = Buffer.from(base64Credentials, "base64").toString("utf-8");
+  const [username, password] = credentials.split(":");
+
+  if (username !== USERNAME || password !== PASSWORD) {
+    res.writeHead(401, { "WWW-Authenticate": 'Basic realm="Secure Area"' });
+    return res.end("Unauthorized");
+  }
+
   if (req.url === "/heartbeat") {
     res.writeHead(200, { "Content-Type": "text/plain" });
     return res.end("Hello World!\n");
